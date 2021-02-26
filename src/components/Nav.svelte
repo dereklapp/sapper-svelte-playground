1ece677cb6b4f6e6712130706b32de3ce03ec711
Derek Lapp <dereklapp@Dereks-MacBook-Pro.local>
2021-02-26 10:28:08 -0400

    Forked default files from sapper template.

---
+++ src/components/Nav.svelte
@@ -0,0 +1,43 @@
+<script>
+	export const items = [
+		{ endpoint: "people", label: "People" },
+		{ endpoint: "films", label: "Films" },
+		{ endpoint: "starships", label: "Starships" },
+		{ endpoint: "vehicles", label: "Vehicles" },
+		{ endpoint: "species", label: "Species" },
+		{ endpoint: "planets", label: "Planets" },
+	]
+	export let segment;
+</script>
+
+<nav>
+	<ul>
+		<li>
+			<a
+				class:selected='{segment === undefined}'
+				href='.'
+				>
+				Home
+			</a>
+		</li>
+		{#each items as item}
+			<li>
+				<a
+					rel=prefetch
+					class:selected='{segment === item.endpoint}'
+					href="{item.endpoint}"
+					>
+					{item.label}
+				</a>
+			</li>
+		{/each}
+		<li>
+			<a
+				class:selected='{segment === "about"}'
+				href='about'
+				>
+				About
+			</a>
+		</li>
+	</ul>
+</nav>
---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+	<title>
+    StarWars
+  </title>
+</svelte:head>
+
+<h1>
+  StarWars
+</h1>
+
+<figure>
+  <img
+    alt='Borat'
+    src='great-success.png'
+    >
+</figure>
+
+<p>
+  <strong>
+    <em>
+      Very nice!
+    </em>
+  </strong>
+</p>

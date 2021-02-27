32e47d7a9a16e587f13e67402b61f048a3695f47
Derek Lapp <dereklapp@Dereks-MacBook-Pro.local>
2021-02-27 09:11:23 -0400

    Removed unused scss.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,17 +1,18 @@
-<script>
-  import Nav from './../components/Nav.svelte';
-  export let segment;
-</script>
-
-<style lang="scss" global>
-  @import "./styles/global.scss";
+<style lang="scss">
+  @import "./styles/conf.scss";
   main {
     height: 100%;
     background-color: $main-bg-color;
+    font-family: $font-stack-helvetica, sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    line-height: $line-height;
+    color: $text-color;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    min-width: rem(320);
   }
 </style>
-
-<Nav {segment}/>
 
 <main>
   <slot></slot>
